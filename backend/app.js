8cf42cdeaf5a0026bc8218841accbc33733bcfe4
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-09-07 10:45:10 +0530

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const bodyParser = require('body-parser');
 
-// const CronJob = require('cron').CronJob;
+const CronJob = require('cron').CronJob;
 
 const sequelize = require('./utils/database');
 
@@ -35,6 +35,7 @@ const { Op } = require('sequelize');
 require('dotenv').config();
 
 app.use(cors({
+    origin: ['http://13.232.181.11:80', 'http://13.232.181.11:4000'],
     credentials: true
 }));
 
@@ -68,28 +69,28 @@ User.hasMany(Members);
 Group.hasMany(Chat);
 Chat.belongsTo(Group);
 
-// var job = new CronJob(
-//     '* * * * * *',
-//     async function() {
-//         const t = await sequelize.transaction();
-//         try {
-//             const oneDayAgo = new Date();
-//             oneDayAgo.setDate(oneDayAgo.getDate() - 1);
-//             const chats = await Chat.findAll({where: {createdAt: {[Op.lt]: oneDayAgo}}});
-//             chats.forEach(async (chat) => {
-//                 await archivedChat.create(chat, {transaction: t});
-//                 await Chat.destroy({where: {id: chat.id}, transaction: t});
-//             });
-//             await t.commit();
-//         } catch (error) {
-//             await t.rollback();
-//             console.log(error.message);
-//         }
-//     },
-//     null,
-//     true,
-//     'Asia/Kolkata'
-// );
+var job = new CronJob(
+    '0 1 * * *',
+    async function() {
+        const t = await sequelize.transaction();
+        try {
+            const oneDayAgo = new Date();
+            oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+            const chats = await Chat.findAll({where: {createdAt: {[Op.lt]: oneDayAgo}}});
+            chats.forEach(async (chat) => {
+                await archivedChat.create(chat, {transaction: t});
+                await Chat.destroy({where: {id: chat.id}, transaction: t});
+            });
+            await t.commit();
+        } catch (error) {
+            await t.rollback();
+            console.log(error.message);
+        }
+    },
+    null,
+    true,
+    'Asia/Kolkata'
+);
 
 sequelize.sync()
 .then(() => {
